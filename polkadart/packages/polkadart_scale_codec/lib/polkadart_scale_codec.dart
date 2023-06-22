47df5cdb2dac7162162bd310b6b74b19a3680980
Simon Evans <v16Studios@users.noreply.github.com>
2023-06-22 03:26:54 +0100

    Add polkadart. (#1)

---
+++ polkadart/packages/polkadart_scale_codec/lib/polkadart_scale_codec.dart
@@ -0,0 +1,4 @@
+library polkadart_scale_codec;
+
+export 'src/core/core.dart';
+export 'src/util/utils.dart';
---
+++ polkadart/packages/ss58_codec/lib/ss58_codec.dart
@@ -0,0 +1,4 @@
+library ss58_codec;
+
+export 'src/ss58_codec_base.dart';
+export 'src/address.dart';
