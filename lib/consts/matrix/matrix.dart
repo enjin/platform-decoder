11049cd1d7e18b311c032b236df8c9c463e692c6
Leonardo Custodio <leonardo@custodio.me>
2024-05-15 11:06:33 -0300

    Update matrix.dart
    Signed-off-by: Leonardo Custodio <leonardo@custodio.me>

--- lib/consts/matrix/matrix.dart
+++ lib/consts/matrix/matrix.dart
@@ -2,9 +2,9 @@ import 'canary/canary.dart' as c;
 import 'production/production.dart' as p;
 
 String canary() {
-  return c.v1005;
+  return c.v1006;
 }
 
 String production() {
-  return p.v1005;
+  return p.v1006;
 }
