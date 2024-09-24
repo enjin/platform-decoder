659aed3ed906c6b2e87719f7802a88cbcbf246fd
Leonardo Custodio <leonardo@custodio.me>
2024-09-24 13:05:55 -0300

    [PLA-2013] Fixes bind port (#42)

--- bin/server.dart
+++ bin/server.dart
@@ -7,7 +7,6 @@ import 'package:platform_decoder/handler/general.dart';
 import 'package:shelf_plus/shelf_plus.dart';
 
 int numberOfIsolates = 8;
-int defaultBindPort = 8090;
 
 final logger = Logger('Decoder');
 
@@ -16,16 +15,13 @@ void loadConfig() {
 
   numberOfIsolates =
       int.tryParse(env.getOrElse('NUMBER_OF_ISOLATES', () => '8')) ?? 8;
-  defaultBindPort =
-      int.tryParse(env.getOrElse('DEFAULT_BIND_PORT', () => '8090')) ?? 8090;
   int specPerIsolate =
       int.tryParse(env.getOrElse('SPEC_PER_ISOLATE', () => '4')) ?? 4;
   int specExpireDuration =
       int.tryParse(env.getOrElse('SPEC_EXPIRE_DURATION', () => '300')) ?? 300;
 
-  logger.info('Starting Platform Decoder v2.1.0');
+  logger.info('Starting Platform Decoder v2.1.1');
   logger.info('Number of isolates: $numberOfIsolates');
-  logger.info('Default bind port: $defaultBindPort');
   logger.info('Spec per isolate: $specPerIsolate');
   logger.info('Spec expire duration: $specExpireDuration');
 }
@@ -48,7 +44,7 @@ void spawnServer(_) => shelfRun(
       init,
       defaultBindAddress: InternetAddress.anyIPv4,
       defaultShared: true,
-      defaultBindPort: defaultBindPort,
+      defaultBindPort: 8090,
       defaultEnableHotReload: false,
     );
 
